1e4e7952729178934de7cbb8be8a5ce5d7a8004d
sabir <sabir01abdel@gmail.com>
2016-06-26 21:25:32 +0100

    first commit

--- messages/fr/app.php
+++ messages/fr/app.php
@@ -61,6 +61,7 @@ return [
     'Ram' => '',
     'Registre' => '',
     'Reset' => '',
+    'Save to PDF' => '',
     'S N' => '',
     'Search' => '',
     'Text' => '',
--- views/interventions/view.php
+++ views/interventions/view.php
@@ -15,6 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
+        <?= Html::a('<i class="fa fa-download"></i>  '.Yii::t('app', 'Save to PDF'), ['savePdf', 'id' => $model->id], ['class' => 'btn btn-success']) ?>
         <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
